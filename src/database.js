92bd732f74fe03047c59a28212a88b9769e751f9
DMAGUILARg <111946899+DMAGUILARg@users.noreply.github.com>
2024-05-26 20:44:31 -0500

    Se realiza un array con los respectivos productos a la licorera

---
+++ src/database.js
@@ -0,0 +1,43 @@
+const licorera =[
+    {
+        nombre: "Johnnie Walker Black Label",
+        tipo: "Whisky",
+        precio: 150000,
+        volumen: "750ml",
+        origen: "Escocia",
+        imagen: "/src/assets/img/Black.jpg"
+    },
+    {
+        nombre: "Ron Viejo de Caldas",
+        tipo:"Ron",
+        precio: 45000,
+        volumen: "1L",
+        origen: "Colombiano",
+        imagen:"/src/assets/img/Ron_Caldas.webp"
+    },
+    {
+        nombre: "Aguardiente Rosado",
+        tipo: "Aguardiente",
+        precio:40000,
+        volumen:"700ml",
+        origen:"Mexicano",
+        imagen:"/src/assets/img/Aguardiente_Rosado.jpg"
+    },
+    {
+        nombre: "Cerveza Aguila",
+        tipo: "Cerveza",
+        precio: 3000,
+        volumen:"330ml",
+        origen: "Barranquillero",
+        imagen:"/src/assets/img/Cervez_Aguila.jpg"
+    },
+    {
+        nombre: "El Enemigo Malbec ",
+        tipo: "Vino",
+        precio: 220.000,
+        volumen: "750ml",
+        origen: "Argentino",
+        imagen: "/src/assets/img/Vino_Malbec.jpg"
+    },
+];
+console.log (licorera);
